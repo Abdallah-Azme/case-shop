c85017990f75151671d5963fdf0cbcc3d836c9ea
Abdallah-Azme <abdallahazme199794@gmail.com>
2024-05-08 14:38:17 +0300

    add landing components and navbar

---
+++ src/lib/data.ts
@@ -0,0 +1,13 @@
+export const featuresList = [
+  "High-quality, durable material",
+  "5 year print guarantee",
+  "Modern iPhone models supported",
+];
+
+export const clientImages = [
+  "/users/user-1.png",
+  "/users/user-2.png",
+  "/users/user-3.png",
+  "/users/user-4.jpg",
+  "/users/user-5.jpg",
+];
